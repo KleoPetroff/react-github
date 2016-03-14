25af35fb69264b5cdf06f61802c6e90e403c0e79
KleoPetroff <kleopetroff@gmail.com>
2016-03-14 21:03:40 +0200

    create NotesList component for rendering the user notes

---
+++ app/components/notes/NotesList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NotesList = React.createClass({
+  render() {
+    var notes = this.props.notes.map((note, index) => {
+      return <li key={index} className="list-group-item">{note['.value']}</li>
+    })
+    return (
+      <ul className="list-group">
+        {notes}
+      </ul>
+    );
+  }
+});
+
+export default NotesList;
