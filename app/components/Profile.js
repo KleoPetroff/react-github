36422a5cee743c556fdff9ebc4abc37d839116d1
KleoPetroff <kleopetroff@gmail.com>
2016-03-14 21:03:04 +0200

    implement firebase and reactfire for notes component

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,8 +3,11 @@ import React from 'react';
 import UserProfile from './github/UserProfile';
 import Repos from './github/Repos';
 import Notes from './notes/Notes';
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 
 const Profile = React.createClass({
+  mixins: [ReactFireMixin],
   getInitialState() {
     return {
       notes: [],
@@ -12,17 +15,26 @@ const Profile = React.createClass({
       repos: []
     };
   },
+  componentDidMount() {
+    this.ref = new Firebase('https://react-github.firebaseio.com/');
+    let childRef = this.ref.child(this.props.params.username);
+    this.bindAsArray(childRef, 'notes');
+  },
+  componentWillUnmount() {
+    this.unbind('notes');
+  },
   render() {
+    console.log(this.ref);
     return (
       <div className="row">
         <div className="col-md-4">
           <UserProfile username={this.props.params.username} bio={this.state.bio} />
         </div>
         <div className="col-md-4">
-          <Repos repos={this.state.repos} />
+          <Repos username={this.props.params.username} repos={this.state.repos} />
         </div>
         <div className="col-md-4">
-          <Notes notes={this.state.notes} />
+          <Notes username={this.props.params.username} notes={this.state.notes} />
         </div>
       </div>
     );
--- app/components/notes/Notes.js
+++ app/components/notes/Notes.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import NotesList from './NotesList';
 
 const Notes = React.createClass({
   render() {
     return (
       <div>
-        User Comments
+        <h3>Notes for {this.props.username}</h3>
+        <NotesList notes={this.props.notes} />
       </div>
     );
   }
