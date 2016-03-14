68e0cd96d14e8c0c7b62f1b3ae8a9786a99ad5f3
KleoPetroff <kleopetroff@gmail.com>
2016-03-15 00:34:45 +0200

    add search box component

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchGithub from './SearchGithub';
 
 var Main = React.createClass({
   render() {
@@ -7,7 +8,7 @@ var Main = React.createClass({
       <div className="main-container">
         <nav className="navbar navbar-default" role="navigation">
           <div className="col-sm-7 col-sm offset-2" style={{marginTop: 15}}>
-            Menu
+            <SearchGithub />
           </div>
         </nav>
       </div>
---
+++ app/components/SearchGithub.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const SearchGithub = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  setRef(ref) {
+    this.usernameRef = ref;
+  },
+  handleSubmit() {
+    console.log(this.context.router);
+    const username = this.usernameRef.value;
+    this.usernameRef.value = '';
+    this.context.router.push('/profile/' + username)
+  },
+  render() {
+    return (
+      <div className="col-sm-12">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group col-sm-7">
+            <input type="text" className="form-control" ref={this.setRef}/>
+          </div>
+          <div className="form-group col-sm-5">
+            <button type="submit" className="btn btn-block btn-primary">Search Github</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+});
+
+export default SearchGithub;
