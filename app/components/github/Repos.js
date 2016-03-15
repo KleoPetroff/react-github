a42a6abc6142843398f28acf542b1a7f15ce3ca5
KleoPetroff <kleopetroff@gmail.com>
2016-03-15 21:10:30 +0200

    repos and user profile components display proper information

--- app/components/github/Repos.js
+++ app/components/github/Repos.js
@@ -6,9 +6,20 @@ const Repos = React.createClass({
     username: React.PropTypes.string.isRequired
   },
   render() {
+    const repoList = this.props.repos.map((repo, index) => {
+      return (
+        <li className="list-group-item" key={index}>
+          {repo.html_url && <h4><a href={repo.html_url} target="_blank">{repo.name}</a></h4>}
+          {repo.description && <p> { repo.description } </p>}
+        </li>
+      )
+    });
     return (
       <div>
-        User Reposs
+        <h3>User Repos </h3>
+        <ul className="list-group">
+          {repoList}
+        </ul>
       </div>
     );
   }
