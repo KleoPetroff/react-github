3befb44cf39d7a3aeef0f57dd0cec99556491d87
KleoPetroff <kleopetroff@gmail.com>
2016-03-16 21:04:12 +0200

    refactor Repos to a es6 class component

--- app/components/github/Repos.js
+++ app/components/github/Repos.js
@@ -1,28 +1,26 @@
 import React from 'react';
 
-const Repos = React.createClass({
-  propTypes: {
-    repos: React.PropTypes.array.isRequired,
-    username: React.PropTypes.string.isRequired
-  },
+class Repos extends React.Component {
   render() {
-    const repoList = this.props.repos.map((repo, index) => {
-      return (
-        <li className="list-group-item" key={index}>
-          {repo.html_url && <h4><a href={repo.html_url} target="_blank">{repo.name}</a></h4>}
-          {repo.description && <p> { repo.description } </p>}
-        </li>
-      )
-    });
     return (
       <div>
         <h3>User Repos </h3>
         <ul className="list-group">
-          {repoList}
+          {this.props.repos.map((repo, index) => (
+            <li className="list-group-item" key={index}>
+              {repo.html_url && <h4><a href={repo.html_url} target="_blank">{repo.name}</a></h4>}
+              {repo.description && <p> { repo.description } </p>}
+            </li>
+          ))}
         </ul>
       </div>
-    );
+    )
   }
-});
+}
+
+Repos.propTypes = {
+  username: React.PropTypes.string.isRequired,
+  repos: React.PropTypes.array.isRequired
+}
 
 export default Repos;
