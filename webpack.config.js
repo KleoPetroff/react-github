402c128d7c18982052da8567543ea7e4d2cecb04
KleoPetroff <kleopetroff@gmail.com>
2016-03-27 22:07:41 +0300

    add new production build and optimized dev webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,54 @@
 var path = require('path');
 
-module.exports = {
-    entry: path.join(__dirname, 'app/index.js'),
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+var config = {
+    devtool: 'source-map',
+
+    entry: path.resolve(__dirname, 'app/index.js'),
+
     output: {
         filename: 'bundle.js',
-        path: path.join(__dirname, 'public')
+        path: path.resolve(__dirname, 'public')
     },
+
     module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            
-            loaders: ['react-hot', 'babel']
-        }]
-    }
-}
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'app'),
+                loaders: ['react-hot', 'babel']
+            },
+            {
+                test: /\.scss$/,
+                include: path.resolve(__dirname, 'app/assets/scss'),
+                loader: ExtractTextPlugin.extract('css!sass')
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url?limit=15000'
+            }
+        ]
+    },
+
+    plugins: [
+        HtmlWebpackPluginConfig,
+        new ExtractTextPlugin('styles/style.css', {
+            allChunks: true
+        }),
+        new OpenBrowserPlugin({
+            url: 'http://localhost:8080'
+        })
+    ]
+};
+
+module.exports = config;
---
+++ webpack.production.config.js
@@ -0,0 +1,53 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+var config = {
+  devtool: 'cheap-module-source-map',
+
+  entry: path.resolve(__dirname, 'app/index.js'),
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        include: path.resolve(__dirname, 'app'),
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'app/assets/scss'),
+        loader: ExtractTextPlugin.extract('css!sass')
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url?limit=15000'
+      }
+    ]
+  },
+
+  plugins: [HtmlWebpackPluginConfig,
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({comments: false}),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: JSON.stringify('production')}
+    }),
+    new ExtractTextPlugin('styles/style.css', {
+      allChunks: true
+    })]
+};
+
+module.exports = config;
